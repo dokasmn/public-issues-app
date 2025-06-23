a6af247391d97ac3136fc2f2ea764916d97192ab
Cauan de Souza Moreira <157118694+dokasmn@users.noreply.github.com>
2025-06-23 19:27:11 -0300

    Update textarea.tsx
    remove props

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,6 +1,9 @@
 import * as React from "react"
 
-export const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(({ className, ...props }, ref) => {
+export const Textarea = React.forwardRef<
+  HTMLTextAreaElement,
+  React.TextareaHTMLAttributes<HTMLTextAreaElement>
+>(({ className, ...props }, ref) => {
   return (
     <textarea
       className={`border border-gray-300 rounded-md p-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
