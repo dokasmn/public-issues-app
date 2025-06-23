0eefc73f6e6658828504b1adb5b13bc3c4334312
Cauan de Souza Moreira <157118694+dokasmn@users.noreply.github.com>
2025-06-23 19:23:04 -0300

    Update textarea.tsx
    remove interface

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,7 +1,5 @@
 import * as React from "react"
 
-export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
-
 export const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(({ className, ...props }, ref) => {
   return (
     <textarea
