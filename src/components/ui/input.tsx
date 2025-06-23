fa0ba9e8f7bbd2b514b990143c8c76f68fd3527b
Cauan de Souza Moreira <157118694+dokasmn@users.noreply.github.com>
2025-06-23 19:22:41 -0300

    Update input.tsx
    remove interface

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,7 +1,5 @@
 import * as React from "react"
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
-
 export const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, ...props }, ref) => {
   return (
     <input
