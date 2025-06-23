1e21ce3fafdffb09a7031fc9ded50143ec398f86
Cauan de Souza Moreira <157118694+dokasmn@users.noreply.github.com>
2025-06-23 19:24:44 -0300

    Update button.tsx
    remove props

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,6 +1,9 @@
 import * as React from "react"
 
-export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ className, ...props }, ref) => {
+export const Button = React.forwardRef<
+  HTMLButtonElement,
+  React.ButtonHTMLAttributes<HTMLButtonElement>
+>(({ className, ...props }, ref) => {
   return (
     <button
       className={`bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 disabled:opacity-50 ${className}`}
@@ -9,5 +12,5 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ classN
     />
   )
 })
+
 Button.displayName = "Button"
-    
