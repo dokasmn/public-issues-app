e9c30fb87e5c9389a663aac59d9313586156b855
Cauan de Souza Moreira <157118694+dokasmn@users.noreply.github.com>
2025-06-23 19:22:22 -0300

    Update button.tsx
    remove interface

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,7 +1,5 @@
 import * as React from "react"
 
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
-
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ className, ...props }, ref) => {
   return (
     <button
